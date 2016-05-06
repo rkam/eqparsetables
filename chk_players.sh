91533dc36e70e1e63f960f72b3b221da7f0074b7
rkam <rkamicar@gmail.com>
2016-05-06 03:28:35 -0700

    Add a couple of shell scripts to help with unknown characters.

---
+++ chk_players.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Run the parser, grab the "can't find these chars list" (if any), and
+#   compare that with all the characters listed in the Roster files, and
+#   show the difference in the format of the config file.
+
+DPS_PARSES_DIR="_dps_parses"
+
+EXISTING_CHARS="_data/config.ini"
+BLACKLIST_FILE="_data/mobs_blacklist.ini"
+
+MISSING_CHARS_FILE="$$.missing"
+OUT_FILE="$$.added"
+
+# shellcheck disable=SC2069
+python3 eqparsedpstables.py "$DPS_PARSES_DIR"/* 2>&1 >/dev/null |
+  grep '^	' | sed -e 's/	//' | sort -u > "$MISSING_CHARS_FILE"
+
+trap 'rm -f $OUT_FILE $MISSING_CHARS_FILE; exit' 1 2 3 15
+
+[[ ! -s $MISSING_CHARS_FILE ]] && echo "No one is missing." && exit 0
+
+ROSTER_FILE=$(./get_players.sh)
+
+fgrep -f "$MISSING_CHARS_FILE" "$ROSTER_FILE" > "$OUT_FILE"
+
+if [[ ! -s $OUT_FILE ]]; then
+  cat $OUT_FILE
+  echo "Cannot find ANY missing in the rosters (pet? mob? => $BLACKLIST_FILE)."
+  cat "$MISSING_CHARS_FILE"
+  exit 1
+fi
+
+# If output is to tty explain, otherwise probably appending to $EXISTING_CHARS
+
+if [[ -t 1 ]]; then
+  echo ""
+  echo "Need to hand-merge these into $EXISTING_CHARS"
+  echo "    # because of the section headers"
+  echo ""
+fi
+
+cat "$OUT_FILE"
+rm "$OUT_FILE"
---
+++ get_players.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Extract all players and their class from RaidRoster dumps
+
+ROSTERS_DIR="./_roster"
+
+OUTFILE="all_players_$(date +'%y%m%d')"
+
+# the != 50 excludes Vox runs
+
+# shellcheck disable=SC2016
+grep -h '^[0-9]' $ROSTERS_DIR/Raid*.txt |
+  ruby -n -e '
+  a = $_.split("\t")
+
+  next if a[2] == "50"
+
+  long = [  "Bard", "Beastlord", "Berserker", "Cleric", "Druid",
+            "Enchanter", "Magician", "Monk", "Necromancer", "Paladin",
+            "Ranger", "Rogue", "Shadow Knight", "Shaman", "Warrior", "Wizard" ]
+
+  short = %w[ BRD BST BER CLR DRU ENC MAG MNK NEC PAL RNG ROG SHD SHA WAR WIZ ]
+
+  i = long.find_index(a[3])
+
+  print("#{a[1]},#{short[i]},#{a[1]}\n")
+' |
+  sort -u -t, -k2,3 > "$OUTFILE"
+
+ls "$OUTFILE"
+
