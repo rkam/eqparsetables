e324269ebc2f3b811340aacd48eb0288348e133a
Andrew Quinn <andrew.quinn@tu-dortmund.de>
2016-05-09 03:05:19 +0200

    add command line args for forcing dps parse and migrate to argparse

--- eqparsetables.py
+++ eqparsetables.py
@@ -3,6 +3,7 @@
 
 import sys
 import getopt
+import argparse
 import os
 import gpcastreader as gpc
 import enjincastprinter as ecp
@@ -23,25 +24,40 @@ def main(argv):
     config_path = cwd + '/config.ini'
     blacklist_path = cwd + '/blacklist.ini'
     input_path = cwd + '/parse.txt'
+    dps = False
+    dps_first = 1
+    dps_last = sys.maxsize
 
-    # parse arguments
-    try:
-        opts, args = getopt.getopt(argv, 'hp:b:', ['parsefile=', 'blacklist='])
-    except getopt.GetoptError:
-        print('myst.py -p <parsefile> -b <blacklist>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('myst.py -p <parsefile> -b <blacklist>')
-            sys.exit()
-        elif opt in ('-p', '--parsefile'):
-            input_path = arg
-        elif opt in ('-b', '--blacklist'):
-            blacklist_path = arg
+    parser = argparse.ArgumentParser(description='Transform GamParse output into you favorite forum table format.')
+    parser.add_argument('-p', '--parsefile', help='path to GamParse output', metavar='PATH')
+    parser.add_argument('-b', '--blacklist', help='path to blacklist', metavar='PATH')
+    parser.add_argument('-c', '--config', help='path to config CSV file', metavar='PATH')
+    parser.add_argument('--dps', action='store_true', help='force dps formatting')
+    parser.add_argument('-f', '--dpsfirst', help='highest ranking dpser to show', metavar='FIRST')
+    parser.add_argument('-l', '--dpslast', help='lowest ranking dpser to show', metavar='LAST')
 
-    reader = gpc.GPCastReader(input_path, config_path, blacklist_path)
-    pdb = parsedb.ParseDB(reader.get_spells_cast_by_class(), reader.classes, reader.caster_dod, reader.config)
-    ecp.print_cast_tables((pdb.get_cast_table(eq_class) for eq_class in sorted(reader.classes)))
+    args = parser.parse_args()
+
+    if args.parsefile:
+        input_path = args.parsefile
+    if args.blacklist:
+        blacklist_path = args.blacklist
+    if args.config:
+        config_path = args.config
+    if args.dps:
+        dps = True
+        if args.dpsfirst:
+            dps_first = int(args.dpsfirst)
+        if args.dpslast:
+            dps_last = int(args.dpslast)
+
+    if dps:
+        reader = gpc.GPDPSReader(input_path, config_path)
+        ecp.print_dps_table(reader.mob, reader.time, reader.guild_stats, reader.dpser_dod, dps_first, dps_last)
+    else:
+        reader = gpc.GPCastReader(input_path, config_path, blacklist_path)
+        pdb = parsedb.ParseDB(reader.get_spells_cast_by_class(), reader.classes, reader.caster_dod, reader.config)
+        ecp.print_cast_tables((pdb.get_cast_table(eq_class) for eq_class in sorted(reader.classes)))
 
 
 if __name__ == '__main__':
