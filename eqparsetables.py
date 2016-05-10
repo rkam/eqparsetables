89d79baa1e0125c6e313e522cdb98ed46146843b
rkam <rkamicar@gmail.com>
2016-05-10 12:46:47 -0700

    Clarified argv description

--- eqparsetables.py
+++ eqparsetables.py
@@ -33,7 +33,7 @@ def main(argv):
     parser.add_argument('-b', '--blacklist', help='path to blacklist', metavar='PATH')
     parser.add_argument('-c', '--config', help='path to config CSV file', metavar='PATH')
     parser.add_argument('--dps', action='store_true', help='force dps formatting')
-    parser.add_argument('--tty', action='store_true', help='output to tty (default to enjin post)')
+    parser.add_argument('--tty', action='store_true', help='output text (default is enjin post format)')
     parser.add_argument('-f', '--dpsfirst', help='highest ranking dpser to show', metavar='FIRST')
     parser.add_argument('-l', '--dpslast', help='lowest ranking dpser to show', metavar='LAST')
 
