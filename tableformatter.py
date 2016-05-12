61014c0b20a8a17d0c0462fbe36f565a270f48ec
Andrew Quinn <andrew.quinn@tu-dortmund.de>
2016-05-12 23:06:45 +0200

    remove unnecessary list construction

--- tableformatter.py
+++ tableformatter.py
@@ -29,17 +29,17 @@ def format_enjin_table(table: parsedb.CastTable):
     row = '[tr][td]{0}[/td][/tr]'
     row_sep = '[/td][td]'
 
-    parse_table = ParseTable()
-    parse_table.title = '[size=5][b]{0}[/b][/size]'.format(table.class_name)
-    parse_table.table_start = '[table]'
-    parse_table.table_stop = '[/table]'
-    parse_table.headers = [header.format(header_sep.join([''] + [x for x in table.get_players()]))]
-    parse_table.headers.append(header.format(header_sep.join(['Total'] + [str(x) for x in table.get_totals()])))
+    pt = ParseTable()
+    pt.title = '[size=5][b]{0}[/b][/size]'.format(table.class_name)
+    pt.table_start = '[table]'
+    pt.table_stop = '[/table]'
+    pt.headers = [header.format(header_sep.join([''] + table.get_players()))]
+    pt.headers.append(header.format(header_sep.join(['Total'] + [str(x) for x in table.get_totals()])))
     spells = table.get_spells()
     t_rows = table.get_rows()
-    parse_table.rows = [row.format(row_sep.join([spells[i]] + [str(x) for x in t_row])) for i, t_row in enumerate(t_rows)]
+    pt.rows = [row.format(row_sep.join([spells[i]] + [str(x) for x in tr])) for i, tr in enumerate(t_rows)]
 
-    return parse_table
+    return pt
 
 
 def humanize(s):
